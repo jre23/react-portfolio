e8213f6981192dab0325ab18dbed3ee5f2b49530
jre23 <jre.estrada@gmail.com>
2021-02-03 15:57:27 -0800

    deleted unnecessary files created with create react app, created skeleton file structure and component files

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainContainer from "./components/MainContainer";
+
+const App = () => {
+  return <MainContainer />;
+}
+
+export default App;
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import API from "../utils/API";
+import About from "./About";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+
+const MainContainer = () => {
+  return (
+    <div>
+      <h1>Hello from the main container!</h1>
+    </div>
+  )
+}
+
+export default MainContainer;
