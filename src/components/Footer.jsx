48c970283ccab15514f4be24eb21971dd7f2e236
jre23 <jre.estrada@gmail.com>
2021-02-03 19:42:56 -0800

    added styles to navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-
-const styles = {
-  background: {
-    backgroundColor: "#c1c8e4"
-  },
-
-  borderPosition: {
-    borderTop: "solid",
-    borderColor: "#314455",
-    borderWidth: "6px",
-    position: "fixed",
-    left: "0",
-    bottom: "0",
-    width: "100%"
-  },
-
-  colors: {
-    color: "rgb(108, 117, 125)"
-  }
-};
+import "../styles/Footer.css"
 
 const Footer = () => {
   return (
-    <footer className="text-center py-1" style={styles.background, styles.borderPosition}>
-      <p className="mb-0 py-2" style={styles.colors}>
+    <footer className="text-center py-1">
+      <p className="mb-0 py-2">
         &copy; Copyright 2020 Joel Estrada
 </p>
     </footer>
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "../styles/NavLinks.css"
 
 const NavLinks = () => {
 
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark nav-portfolio">
-      <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+    <nav className={location.pathname === "/" ? "navbar navbar-expand-lg navbar-dark nav-index" : location.pathname === "/portfolio" ? "navbar navbar-expand-lg navbar-dark nav-portfolio" : location.pathname === "/contact" ? "navbar navbar-expand-lg navbar-dark nav-contact" : "navbar navbar-expand-lg navbar-dark"}>
+      <Link to="/" className="navbar-brand">
         Joel Estrada
       </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
@@ -18,17 +19,17 @@ const NavLinks = () => {
       <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
           <li className="nav-item">
-            <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active" : "nav-link"}>
+            <Link to="/contact" className={location.pathname === "/contact" ? "nav-link active nav-contact" : "nav-link"}>
               Contact
-                </Link>
+            </Link>
           </li>
           <li className="nav-item active">
-            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>
+            <Link to="/portfolio" className={location.pathname === "/portfolio" ? "nav-link active nav-portfolio" : "nav-link"}>
               Portfolio
                 </Link>
           </li>
           <li className="nav-item">
-            <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+            <Link to="/" className={location.pathname === "/" ? "nav-link active nav-index" : "nav-link"}>
               About
                 </Link>
           </li>
