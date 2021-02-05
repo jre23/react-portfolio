f3a26a5aa70e1b27c8f772d9efd4e96be1d9ad95
jre23 <jre.estrada@gmail.com>
2021-02-05 15:35:46 -0800

    removed extra screenshots from the public/images directory, added 404.html file and scripts for single page react app to be hosted on github pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/react-portfolio">
       <div>
         <NavLinks />
         <Switch>
