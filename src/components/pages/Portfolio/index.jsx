60f71b76e78c1850b2c664517f6a8c65230134d4
jre23 <jre.estrada@gmail.com>
2021-05-12 00:44:20 -0700

    debugging image sources

--- src/components/pages/Portfolio/index.jsx
+++ src/components/pages/Portfolio/index.jsx
@@ -4,6 +4,13 @@ import PortfolioModal from "./PortfolioModal";
 import projects from "./projects.json";
 import "../../../styles/Portfolio.css";
 
+import projectImg0 from "../../../images/bear-share.png";
+import projectImg1 from "../../../images/the-night-planner.png";
+import projectImg2 from "../../../images/google-books-search.png";
+import projectImg3 from "../../../images/employee-directory.png";
+import projectImg4 from "../../../images/party-perfect.png";
+import projectImg5 from "../../../images/budget-tracker.png";
+
 const Portfolio = () => {
   const [clickState, setClickState] = useState({
     projectTitle: "",
@@ -19,8 +26,12 @@ const Portfolio = () => {
   });
 
   const displayProjectInfo = (event) => {
+    let source = event.target.src.slice(21);
     let dataValue = parseInt(event.target.getAttribute("data-value"));
-    setClickState({ ...projects[dataValue] });
+    setClickState({
+      ...projects[dataValue],
+      imgSrc: source,
+    });
   };
 
   return (
